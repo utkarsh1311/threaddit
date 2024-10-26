b1d6e2b3bc965580a91162c38f0d56ec7d99d189
utkarsh maurya <itsutkarsh123@gmail.com>
2024-10-26 20:36:25 +0530

    feat: Add AddSubReddit component and dropdown menu in SubRedditView component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
-import { Button } from "./components/ui/button";
-import { Plus } from "lucide-react";
-import { ScrollArea } from "./components/ui/scroll-area";
+import AddSubReddit from "./components/AddSubReddit";
 import SubRedditView from "./components/SubRedditView";
+import { Button } from "./components/ui/button";
 
 const App = () => {
 	return (
-		<div className="grid p-4 place-content-center min-h-screen  grid-cols-1 grid-rows-1 font-primary">
+		<div className="grid p-4 place-content-center min-h-screen grid-cols-1 grid-rows-1 font-primary">
 			<div className="flex flex-col gap-4 relative">
-				<header className="flex justify-between p-4  ">
-					<span className="text-6xl font-extrabold">Threadit</span>
+				<header className="flex justify-between p-4">
+					<span className="text-6xl font-extrabold text-zinc-900">
+						Threaddit
+					</span>
 					<Button variant={"ghost"} className="rounded-full h-14 w-14">
 						<GitHubLogoIcon className="scale-[2]" />
 					</Button>
 				</header>
-				<ScrollArea className=" flex-grow p-2 rounded-lg border bg-card text-card-foreground shadow-sm bg-neutral-50">
-					<div className="h-full flex  scrollbar-hide snap-x snap-mandatory  overflow-y-auto ">
+				<div className="flex-grow p-2 bg-card text-card-foreground bg-zinc-50 rounded-md border shadow-sm">
+					<div className="h-[calc(100vh-10rem)] flex overflow-x-auto overflow-y-hidden scrollbar-hide snap-x snap-mandatory  rounded-md">
+						<SubRedditView />
+						<SubRedditView />
+						<SubRedditView />
 						<SubRedditView />
 						<SubRedditView />
 					</div>
-				</ScrollArea>
-				<Button className="absolute bottom-10 right-10 h-16 w-16 rounded-full shadow-md">
-					<Plus />
-				</Button>
+				</div>
+				<AddSubReddit />
 			</div>
 		</div>
 	);
