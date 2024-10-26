cb4f3b17e1b9bceeefcb9f4d18407b6aa8829f7b
utkarsh maurya <itsutkarsh123@gmail.com>
2024-10-26 22:49:54 +0530

    feat: Add persisting query client to local storage

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,29 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Add this import
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
 			gcTime: 1000 * 60 * 60,
+			// Add this option
+			staleTime: 1000 * 60 * 5, // 5 minutes
 		},
 	},
 });
 
+// Add this code
+const localStoragePersister = createSyncStoragePersister({
+	storage: window.localStorage,
+});
+
+persistQueryClient({
+	queryClient,
+	persister: localStoragePersister,
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
@@ -21,4 +35,3 @@ createRoot(document.getElementById("root")!).render(
 		</QueryClientProvider>
 	</StrictMode>
 );
-
