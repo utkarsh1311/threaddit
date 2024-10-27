e7c0906925c36fe52c8c88aab2fab5ffe3bd1b08
utkarsh maurya <itsutkarsh123@gmail.com>
2024-10-27 23:06:31 +0530

    refactor: Update BASE_URL in posts.ts to remove trailing slash

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://www.reddit.com/r/";
+const BASE_URL = "https://www.reddit.com/r";
 
 const getSubRedditPosts = async (subreddit: string) => {
 	const response = await fetch(`${BASE_URL}/${subreddit}.json`);
