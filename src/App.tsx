54334ee00c0bed3475a3a5eddbf7b828ab19965e
utkarsh maurya <itsutkarsh123@gmail.com>
2024-10-27 20:16:04 +0530

    feat: Add react-transition-group package for animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import AddSubReddit from "./components/AddSubReddit";
 import Header from "./components/Header";
 import SubReddit from "./components/SubReddit";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const App = () => {
 	const [subReddits, setSubReddits] = useState<string[]>(
 		JSON.parse(localStorage.getItem("subReddits") || "[]")
 	);
+
 
 	return (
 		<div className="grid p-4 place-content-center min-h-screen grid-cols-1 grid-rows-1 font-primary">
@@ -15,13 +17,22 @@ const App = () => {
 				<div className="flex-grow p-2 bg-card text-card-foreground bg-zinc-50 rounded-md border shadow-sm">
 					<div className="h-[calc(100vh-10rem)] flex overflow-x-auto overflow-y-hidden scrollbar-hide snap-x snap-mandatory  rounded-md">
 						{subReddits.length === 0 && (
-							<div className="flex items-center justify-center w-full">
-								<h1 className="text-2xl text-gray-500">No subreddits added</h1>
+							<div className="absolute inset-0 flex flex-col items-center justify-center w-full h-full">
+								<h2 className="text-2xl font-bold">No Subreddits added</h2>
+								<p className="text-lg">Add a subreddit to get started</p>
 							</div>
 						)}
-						{subReddits.map((subReddit: string) => (
-							<SubReddit key={subReddit} subReddit={subReddit} />
-						))}
+						<TransitionGroup component={null}>
+							{subReddits.map((subReddit: string) => (
+								<CSSTransition key={subReddit} timeout={400} classNames="item">
+									<SubReddit
+										key={subReddit}
+										subReddit={subReddit}
+										setSubReddits={setSubReddits}
+									/>
+								</CSSTransition>
+							))}
+						</TransitionGroup>
 					</div>
 				</div>
 				<AddSubReddit setSubreddits={setSubReddits} />
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -10,7 +10,7 @@ function Post({ title, author, timestamp }: PostProps) {
 		<div className="shadow-sm border rounded-md p-4 bg-white hover:shadow-md transition-shadow">
 			<h2 className="text-lg font-semibold mb-2 text-zinc-700">{title}</h2>
 			<div className="flex justify-between items-center text-xs text-gray-500">
-				<span>{"u/" + author}</span>
+				<span className="">{"u/" + author}</span>
 				<span>{new Date(timestamp * 1000).toLocaleString()}</span>
 			</div>
 		</div>
